09c754b2a29802e6bb3bc21539c89858a32382ee
Snash <raulmv01@hotmail.es>
2020-01-27 18:15:53 +0100

    proxy & api tested

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 
+
+const axiosInstance = axios.create({
+  baseURL: 'http://jsonplaceholder.typicode.com'
+})
+
 export default class App extends Component {
   constructor(){
     super();
@@ -10,10 +15,10 @@ export default class App extends Component {
   }
   async componentDidMount(){
     try {
-      const res = await axios.get('http://localhost:5000/hello');
+      const res = await axiosInstance.get('/posts');
       this.setState({
         hello: res.data
-      })   
+      })    
     } 
     catch (error) {
       console.log(error.message);
@@ -22,7 +27,10 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        {this.state.hello !== null && <h1>{this.state.hello}</h1>}
+        {this.state.hello !== null && (
+        <ul>
+          {this.state.hello.map( el => <li key={el.id}>{el.title}</li>)}
+        </ul>)}
       </div>
     )
   }
